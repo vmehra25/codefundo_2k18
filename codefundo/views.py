8ec782554275c7c196d2ea56292c22be47af496c
unknown <mananpoddarcse@gmail.com>
2018-10-25 00:26:09 +0530

    let's merge

--- codefundo/views.py
+++ codefundo/views.py
@@ -16,38 +16,55 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 form=input()
 def main_page(request):
     form=input()
+    total_fund = gov_fund.objects.all()
+    s=0
+    for i in total_fund:
+        s=s+i.fund
+    
     #mypaste=user_details.objects.all()
     #mypaste = sorted(mypaste,key=lambda x:x.url,reverse=True)
-    count_user=track_users()
+    t=0
+    t_obj=0
+    count_user=track_users.objects.all()
+    for i in count_user:
+        t = t + i.user_count
+        t_obj = t_obj + 1
+    print(t)
+    per_person_fund = s/t
     if request.method=="POST":
-          form=input(request.POST)
+        form=input(request.POST)
          # myd
-          current=form.save(commit=False)
-          try:
-             content_object=track_users.objects.get(pincode = current.pincode)
-          except:
-             content_object = None
-          if content_object == None:
-             content_object = track_users()
-             content_object.pincode=current.pincode
-             content_object.city=current.city
+        current=form.save(commit=False)
+        try:
+            content_object=track_users.objects.get(pincode = current.pincode)
+        except:
+            content_object = None
+        if content_object == None:
+            content_object = track_users()
+            content_object.pincode=current.pincode
+            content_object.city=current.city
              
-             content_object.user_count=1
-             content_object.save()
-          else :
-              content_object.user_count=content_object.user_count+1
-              content_object.save()
+            content_object.user_count=1
+            content_object.fund = per_person_fund * content_object.user_count
+            content_object.save()
+        else :
+            content_object.user_count=content_object.user_count+1
+            content_object.fund = per_person_fund * content_object.user_count
+
+            content_object.save()
          
           
          
             
-          if form.is_valid():
+        if form.is_valid():
             
-            form.save()
-            return render(request,"codefundo/ss.html") 
-          else:
-               return render(request,"codefundo/return_valid_data.html")   
-    return render(request,"codefundo/user_detail.html",{"form":form} )
+         form.save()
+         return render(request,"codefundo/registered.html",{"form":form,"objects":count_user,}) 
+        else:
+         return render(request,"codefundo/return_valid_data.html")   
+    count_user=track_users.objects.all()
+
+    return render(request,"codefundo/home.html",{"form":form,"total":t_obj,"objects":count_user,"itr":0} )
 
 def main_log_page(request):
     form=fund_gov()
